161275f6a2657df367417c23689f2d6e1466a3a5
Xavier Perseguers <xavier@causal.ch>
2023-10-25 15:34:38 +0200

    [TASK] Hide useless configuration fields for the plugin

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -7,3 +7,7 @@ defined('TYPO3') || die();
     'Setup',
     'LLL:EXT:mfa_frontend/Resources/Private/Language/locallang_db.xlf:plugin.setup'
 );
+
+$pluginSignature = 'mfafrontend_setup';
+// Disable the display of page fields
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist'][$pluginSignature] = 'pages,recursive';
