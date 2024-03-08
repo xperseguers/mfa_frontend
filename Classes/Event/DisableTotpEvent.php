ef079e4ef6f8026b6997e07b7e06c65fa47e1b82
Xavier Perseguers <xavier@causal.ch>
2024-03-08 18:18:53 +0100

    [FEATURE] Add PSR-14 event to let 3rd-party ext disable MFA
    Usually, in order to disable MFA for a given record, a valid
    OTP must be provided.
    
    The event DisableTotpEvent can be used to bypass this check
    based on any custom business logic.
    
    By default TYPO3 administrators are allowed to disable MFA
    for any record.

---
+++ Classes/Event/DisableTotpEvent.php
@@ -0,0 +1,56 @@
+<?php
+declare(strict_types=1);
+
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 3
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+namespace Causal\MfaFrontend\Event;
+
+use Causal\MfaFrontend\Domain\Model\FrontendUser;
+
+final class DisableTotpEvent
+{
+    private string $table;
+
+    private int $uid;
+
+    private bool $bypassValidation;
+
+    public function __construct(string $table, int $uid, bool $bypassValidation)
+    {
+        $this->table = $table;
+        $this->uid = $uid;
+        $this->bypassValidation = $bypassValidation;
+    }
+
+    public function getTable(): string
+    {
+        return $this->table;
+    }
+
+    public function getUid(): int
+    {
+        return $this->uid;
+    }
+
+    public function getBypassValidation(): bool
+    {
+        return $this->bypassValidation;
+    }
+
+    public function setBypassValidation(bool $bypassValidation): self
+    {
+        $this->bypassValidation = $bypassValidation;
+        return $this;
+    }
+}
