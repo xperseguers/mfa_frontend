5d44744459a1aa8e7c6e94e2557aa53fcb699ece
Xavier Perseguers <xavier@causal.ch>
2024-06-05 10:52:06 +0200

    [TASK] Drop remaining code supporting PHP 7.4

--- Classes/Validation/Validator/SetupFormValidator.php
+++ Classes/Validation/Validator/SetupFormValidator.php
@@ -16,29 +16,17 @@ declare(strict_types=1);
 
 namespace Causal\MfaFrontend\Validation\Validator;
 
-use Causal\MfaFrontend\Validation\Validator\Traits\SetupFormValidatorPhp74Trait;
 use Causal\MfaFrontend\Validation\Validator\Traits\SetupFormValidatorPhp8Trait;
 use TYPO3\CMS\Core\Information\Typo3Version;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Extbase\Validation\Validator\AbstractGenericObjectValidator;
 use TYPO3\CMS\Extbase\Validation\Validator\GenericObjectValidator;
 
 if ((new Typo3Version())->getMajorVersion() >= 12) {
-    class ParentValidatorClass extends AbstractGenericObjectValidator {}
-} else {
-    class ParentValidatorClass extends GenericObjectValidator {}
-}
-
-if (\PHP_VERSION_ID < 80000) {
-    // TYPO3 v10 or v11 with PHP 7.4
-    class SetupFormValidator extends ParentValidatorClass
-    {
-        use SetupFormValidatorPhp74Trait;
+    class SetupFormValidator extends AbstractGenericObjectValidator {
+        use SetupFormValidatorPhp8Trait;
     }
 } else {
-    // TYPO3 v11 or v12 with PHP 8+
-    class SetupFormValidator extends ParentValidatorClass
-    {
+    class SetupFormValidator extends GenericObjectValidator {
         use SetupFormValidatorPhp8Trait;
     }
 }
