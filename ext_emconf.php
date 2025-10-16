dcf3610fca71f49156a6cba535eb9f031e1522f0
Xavier Perseguers <xavier@causal.ch>
2025-10-16 11:33:11 +0200

    [TASK] Mark early compatibility with PHP 8.5 (RC1)
    Related: #8

--- ext_emconf.php
+++ ext_emconf.php
@@ -18,7 +18,7 @@ $EM_CONF[$_EXTKEY] = [
     'author_company' => 'Causal Sàrl',
     'constraints' => [
         'depends' => [
-            'php' => '8.0.0-8.3.99',
+            'php' => '8.0.0-8.5.99',
             'typo3' => '11.5.0-13.4.99'
         ],
         'conflicts' => [],
